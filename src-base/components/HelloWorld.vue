f8ba03e80bea5dbc28e520b0a460f6e73e8290b5
HK <2971913255@qq.com>
2021-05-17 14:36:22 +0800

    todo-list-版本1

---
+++ src-base/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p class="msg">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg:'hello Vue Component'
+    }
+  }
+}
+</script>
+
+<style>
+  .msg{
+    font-size: 30px;
+    background-color: #AA7890;
+  }
+</style>
