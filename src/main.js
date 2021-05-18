218c6a83a8301cf1a86cdcdc9373ccc34eaf3744
HK <2971913255@qq.com>
2021-05-18 14:15:55 +0800

    todo-list-版本2

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 
-import './base.css'
-
-new Vue({
+new Vue({  //配置对象的属性名是固定的
   el:'#app',
   components:{App},
-  template:'<App/>'
+  template:'<App/>',
+  router
 })
